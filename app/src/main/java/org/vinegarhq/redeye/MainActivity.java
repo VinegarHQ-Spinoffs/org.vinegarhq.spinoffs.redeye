709e043d99c65b16a4d8c2eb7e48caee7eab24b1
lunarlattice <lunar.lattice0@gmail.com>
2024-02-01 17:01:11 -0800

    Check for camera permissions

--- app/src/main/java/org/vinegarhq/redeye/MainActivity.java
+++ app/src/main/java/org/vinegarhq/redeye/MainActivity.java
@@ -1,19 +1,12 @@
 package org.vinegarhq.redeye;
 
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
