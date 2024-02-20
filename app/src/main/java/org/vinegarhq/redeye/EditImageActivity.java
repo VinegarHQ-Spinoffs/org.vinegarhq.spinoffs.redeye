affd9b177328d77acc99e0b3768cf194c2ff3bfc
Thelolguy1 <lunar.lattice0@gmail.com>
2024-02-19 22:05:51 -0800

    Add rotate button

--- app/src/main/java/org/vinegarhq/redeye/EditImageActivity.java
+++ app/src/main/java/org/vinegarhq/redeye/EditImageActivity.java
@@ -2,15 +2,21 @@ package org.vinegarhq.redeye;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 
+import androidx.annotation.FloatRange;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.canhub.cropper.CropImageView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.io.File;
 import java.util.Objects;
+
+
 
 public class EditImageActivity extends AppCompatActivity {
 
@@ -19,6 +25,7 @@ public class EditImageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_image);
 
+        // Receive files from intent.
         Uri imageUri;
         File imageFile;
         File cachePath = null;
@@ -31,16 +38,31 @@ public class EditImageActivity extends AppCompatActivity {
             throw new RuntimeException();
         }
 
+
+        // Set up crop image view.
         CropImageView civ = findViewById(R.id.cropimageView);
         civ.setImageUriAsync(imageUri);
 
-        SeekBar onionBar = findViewById(R.id.onionBar);
+        // Set up rotate button modes.
+        FloatingActionButton rotateButton = findViewById(R.id.rotateButton);
+
+        // yes i am aware enums exist, but at the cost of performance.
+        rotateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                civ.rotateImage(90); // TODO: figure out how to do sub-90 rotations without breaking crop
+            }
+        });
+
+        // Set up multiPurposeBar.
+        SeekBar multiPurposeBar = findViewById(R.id.multiPurposeBar);
         ImageView onionImage = findViewById(R.id.onionImage);
-        onionBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+
+        multiPurposeBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                onionImage.setImageAlpha((int) Math.floor(255 * (progress / 100.0)));
-
+                    onionImage.setAlpha((float)(progress / 100.0));
             }
 
             @Override
